fce67a15acf717899cd4e32c22b7391db176e67f
ccurme <chester.curme@gmail.com>
2026-03-24 16:41:19 -0400

    release(together): 0.4.0 (#34)

--- libs/together/tests/integration_tests/test_chat_models.py
+++ libs/together/tests/integration_tests/test_chat_models.py
@@ -17,7 +17,7 @@ def test_chat_together_system_message() -> None:
     chat = ChatTogether(max_tokens=10)
     system_message = SystemMessage(content="You are to chat with the user.")
     human_message = HumanMessage(content="Hello")
-    response = chat([system_message, human_message])
+    response = chat.invoke([system_message, human_message])
     assert isinstance(response, BaseMessage)
     assert isinstance(response.content, str)
 
